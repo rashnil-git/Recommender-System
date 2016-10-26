7538b7999be30debebf5725fa734be7abf0c18fd
Rashnil2016 <rashnil@live.unc.edu>
2016-10-26 01:19:33 -0400

    Recommender System using Apache Spark Initial Commit.

---
+++ data.loader/src/MovieLensDataLoader.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Rash on 26-10-2016.
+ */
+public class MovieLensDataLoader {
+
+
+
+    public void loadMovieData()
+    {
+
+    }
+
+    public void loadRatingsData()
+    {
+
+    }
+
+    public void loadUserTagData()
+    {
+
+    }
+
+    public static void main(String args[])
+    {
+
+    }
+
+}
